e2ba9ac03817a40546df48fb12398435b83556da
André Felipe Dilly Steiger <imzdede79@gmail.com>
2025-11-29 23:45:27 -0300

    middlewares e update de usuario

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { UserController } from "../controllers/user.controllers";
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 const router = Router();
 const userController = new UserController();
 
-router.post('/', userController.create);
 router.post('/login', userController.login);
+
+router.use(authMiddleware);
+
+router.post('/', userController.create);
+router.put('/:id', userController.update);
 
 export default router;
